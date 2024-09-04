2a75e94a20c8482eca57ed6a9afbd35b5ade0c0a
Bilal Yaver <bilalyaver@outlook.com>
2024-09-04 22:13:03 +0300

    chore: Update package.json version to 0.0.5 and webpack.config.js output filename to 'bundle.js'

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,10 @@
+const path = require('path');
+
 module.exports = {
-    module: {
-      rules: [
-        {
-          test: /\.(js|jsx)$/, // Hem .js hem de .jsx dosyalarını işlemeye yönelik kural
-          exclude: /node_modules/,
-          use: {
-            loader: 'babel-loader',
-            options: {
-              presets: ['@babel/preset-env', '@babel/preset-react']
-            }
-          }
-        }
-      ]
-    },
-    resolve: {
-      extensions: ['.js', '.jsx'] // .js ve .jsx uzantılarını tanıyacak
-    }
-  };
+  output: {
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [{ test: /\.txt$/, use: 'raw-loader' }],
+  },
+};
